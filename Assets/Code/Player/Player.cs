dc5b138868ec3cef47af3f481848fae197c334e2
Thomas <segfault.github@gmail.com>
2022-09-15 17:07:32 +0200

    Added map system

---
+++ Assets/Code/Player/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Door/Left")) {
+            Map map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+            map.ChangeRoom(Direction.Left);
+            return;
+        } else if (collider.CompareTag("Door/Right")) {
+            Map map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+            map.ChangeRoom(Direction.Right);
+            return;
+        } else if (collider.CompareTag("Door/Up")) {
+            Map map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+            map.ChangeRoom(Direction.Up);
+            return;
+        } else if (collider.CompareTag("Door/Down")) {
+            Map map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+            map.ChangeRoom(Direction.Down);
+            return;
+        }
+    }
+}
